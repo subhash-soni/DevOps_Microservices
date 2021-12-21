4dc03ad00ee0f19a9b10c110859c1f0b2f21996c
Subh <subh@soni.com>
2021-12-21 14:08:38 +0000

    minikube issue: RC_INSUFFICIENT_CORES: Requested cpu count 2  greater than the available cpus of 1

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+docker tag p4udacity-image:v1 dockerss1/udacity:p4udacity
+dockerpath=dockerss1/udacity:p4udacity
 
 # Step 2:  
 # Authenticate & tag
+docker login -u dockerss1
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push dockerss1/udacity:p4udacity
